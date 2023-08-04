6c037510de7fc8c23b724e90a93f4c6e1c1c0409
se0jinYoon <tjwls1999@gmail.com>
2023-08-04 14:56:47 +0900

    feat: Header fin

--- food-delivery/src/components/Layout/Header.js
+++ food-delivery/src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import { Fragment } from 'react';
 
 import mealsImage from '../../assets/meals.jpeg';
 import classes from './Header.module.css'
+import HeaderCartButoon from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButoon />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />

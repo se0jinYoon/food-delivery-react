9c128c3b3fff4dd334b0a9b7e311dfe84e15381c
se0jinYoon <tjwls1999@gmail.com>
2023-08-05 23:22:28 +0900

    fix: 렌더링 에러..

--- food-delivery/src/App.js
+++ food-delivery/src/App.js
@@ -1,28 +1,29 @@
-import {Fragment, useState} from 'react';
+import { useState } from 'react';
 
-import Header from "./components/Layout/Header";
+import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-  const [cartIsShown ,setCartIsShown] = useState(false);
+  const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
     setCartIsShown(true);
-  }
+  };
 
   const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
 
   return (
-    <Fragment>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
-      <Header onShowCart = {showCartHandler}/>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 

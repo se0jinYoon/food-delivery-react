f2f2fdad2dad7ab18ce214a5a48cfd311b70c93e
se0jinYoon <tjwls1999@gmail.com>
2023-08-05 22:10:09 +0900

    feat: 장바구니 모달창 닫기

--- food-delivery/src/components/Layout/HeaderCartButton.js
+++ food-delivery/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,16 @@
- import CartIcon from "../Cart/CartIcon";
- import classes from './HeaderCartButton.module.css'
- 
- const HeaderCartButoon = props => {
-    return <button className={classes.button}> 
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>
-            3
-        </span>
-    </button>
- }
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css';
 
- export default HeaderCartButoon;
+const HeaderCartButoon = (props) => {
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+};
+
+export default HeaderCartButoon;

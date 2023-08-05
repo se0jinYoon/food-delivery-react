54f280567def64e979d6bfaa2b0918a31356445a
se0jinYoon <tjwls1999@gmail.com>
2023-08-06 00:36:42 +0900

    feat: 장바구니 갯수 업데이트

--- food-delivery/src/components/Cart/Cart.js
+++ food-delivery/src/components/Cart/Cart.js
@@ -1,10 +1,14 @@
-import classes from './Cart.module.css';
+import { useContext } from 'react';
 
+import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
+import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+
   const cartItems = <ul className={classes['cart-items']}>
-    {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+    {CartCtx.map((item) => (
         <li>{item.name}</li>
     ))}
 </ul>;
